5318316dce437e68c7243acea6cdb5d11a924381
Gerbton Fonseca <gerbton@gmail.com>
2021-10-05 07:37:46 -0300

    chore: adds vendor autoload support

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '13ef0569b62fb76d1ca2b7e828f362249581509a',
+    'name' => 'gerbton/fuerza-cursos',
+  ),
+  'versions' => 
+  array (
+    'gerbton/fuerza-cursos' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '13ef0569b62fb76d1ca2b7e828f362249581509a',
+    ),
+    'roave/security-advisories' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0488e161600117fc3a0d72397dad154729002f54',
+    ),
+  ),
+);
