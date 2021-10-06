c52e7053f49881a997d99416ee02bb5b920513bc
Gerbton Fonseca <gerbton@gmail.com>
2021-10-06 15:36:06 -0300

    fix: fixes bug on slug

--- App/Models/PostType/PostTypeSettings.php
+++ App/Models/PostType/PostTypeSettings.php
@@ -29,7 +29,7 @@ class PostTypeSettings {
 		$this->icon           = $icon;
 		$this->description    = '';
 		$this->supports       = [ 'title', 'editor', 'thumbnail' ];
-		$this->rewrite        = [ 'slug' => true ];
+		$this->rewrite        = [];
 		$this->taxonomies     = [];
 		$this->public         = true;
 		$this->showUI         = true;
