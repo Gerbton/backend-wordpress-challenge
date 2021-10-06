5d9f64aba805db56b36ed23e3561d34ebd64b4c4
Gerbton Fonseca <gerbton@gmail.com>
2021-10-06 15:41:21 -0300

    feat: add route rest api support

--- App/Models/Route.php
+++ App/Models/Route.php
@@ -19,4 +19,22 @@ class Route {
 			}
 		);
 	}
+
+	public static function post( string $route, callable $callback ): void {
+		add_action( 'rest_api_init', static function () use ( $route, $callback ) {
+			self::register( \WP_REST_Server::CREATABLE, $route, $callback );
+		} );
+	}
+
+	private static function register( string $method, string $route, callable $callback ): void {
+		register_rest_route(
+			FUERZA_POST_TYPE . '/v1',
+			$route,
+			[
+				'methods'             => $method,
+				'callback'            => $callback,
+				'permission_callback' => '__return_true'
+			]
+		);
+	}
 }
