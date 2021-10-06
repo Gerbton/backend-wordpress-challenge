d73d6b1ae2ce15065d46a66a614afe93d51f9c9b
Gerbton Fonseca <gerbton@gmail.com>
2021-10-06 06:28:28 -0300

    feat: add files to implements the post single

---
+++ Views/Public/Single/content-header.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @var \App\Models\PostType\Post $post
+ **/
+?>
+<div class="fc-container">
+    <div class="fc-d-flex fc-d-flex--justify-center">
+        <p>Carga horária: <span><?= $post->getWorkload() ?></span></p>
+        <p>
+			<?php if ( $post->isOpenToSubscribe() ) { ?>
+                Inscrições até: <span><?= $post->getDeadline() ?></span>
+			<?php } else { ?>
+                Incrições encerradas
+			<?php } ?>
+        </p>
+    </div>
+</div>
