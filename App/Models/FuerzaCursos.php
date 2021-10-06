cbf6d7a39d54492d7f1d246421598a28aab5cec3
Gerbton Fonseca <gerbton@gmail.com>
2021-10-06 00:23:19 -0300

    feat: show fields in admin post type

--- App/Models/FuerzaCursos.php
+++ App/Models/FuerzaCursos.php
@@ -71,4 +71,8 @@ class FuerzaCursos {
 
 		$postTypeBuilder->onSave( [ PostTypeController::class, 'save' ] );
 	}
+
+	public static function showFields( $post ): void {
+		include FUERZA_PLUGIN_DIR . "/Views/Admin/fields.php";
+	}
 }
