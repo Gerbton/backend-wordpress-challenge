3af4e8a1218767bc31017dcc7f259da00ee659e6
Gerbton Fonseca <gerbton@gmail.com>
2021-10-06 15:37:05 -0300

    feat: register scripts

--- App/Models/Asset.php
+++ App/Models/Asset.php
@@ -10,12 +10,39 @@ class Asset {
 		add_action( 'wp_enqueue_scripts', [ $this, 'enqueueStyles' ] );
 	}
 
+	public function registerScripts(): void {
+		add_action( 'wp_enqueue_scripts', [ $this, 'enqueueScripts' ] );
+	}
+
 	public function enqueueStyles(): void {
 		wp_enqueue_style(
 			'Fuerza',
 			FUERZA_PLUGIN_URL . "/assets/css/fuerza.css",
 			[],
 			FUERZA_PLUGIN_VERSION
+		);
+	}
+
+	public function enqueueScripts(): void {
+		wp_enqueue_script(
+			'FCSubscription',
+			FUERZA_PLUGIN_URL . "/assets/js/Subscription.js",
+			[ 'jquery' ],
+			FUERZA_PLUGIN_VERSION,
+			true
+		);
+
+		wp_localize_script(
+			'FCSubscription',
+			'subscribeData',
+			[
+				'endpoints' => [
+					'create' => [
+						'url'    => rest_url( FUERZA_POST_TYPE . '/v1/subscribe' ),
+						'method' => 'POST'
+					]
+				]
+			]
 		);
 	}
 }
