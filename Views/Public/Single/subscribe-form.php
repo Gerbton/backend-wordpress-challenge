97294dbc4f61664bdcb2866d4fcb8a628f860db6
Gerbton Fonseca <gerbton@gmail.com>
2021-10-07 02:47:01 -0300

    feat: improve subscribe form

--- Views/Public/Single/subscribe-form.php
+++ Views/Public/Single/subscribe-form.php
@@ -1,16 +1,26 @@
-<h2 class="fc-title fc-title--form">Tenho interesse</h2>
-<form action="" class="fc-d-flex" id="jsFuerzaSubscribeForm">
-	<input type="hidden" name="fc_nonce" value="<?= wp_create_nonce( 'wp_rest' ) ?>">
+<form class="fc-form" id="jsFuerzaSubscribeForm">
+    <h2 class="fc-title fc-title--form">Tenho interesse</h2>
+
+    <input type="hidden" name="_wpnonce" value="<?= wp_create_nonce( 'wp_rest' ) ?>">
 	<input type="hidden" name="post_id" id="fc_post_id" value="<?= $_post->getID() ?>">
-	<div class="fc-group">
-		<label for="fc_name">Nome</label>
-		<input type="text" name="name" id="fc_name" class="fc_input" placeholder="Jonh Due">
-	</div>
-	<div class="fc-group">
-		<label for="fc_email">Email</label>
-		<input type="email" name="email" id="fc_email" class="fc_input" placeholder="jonh@email.com">
-	</div>
-	<div class="fc-group">
-		<button type="submit">Cadastrar</button>
-	</div>
+
+    <div class="col-12">
+        <div class="fc-group">
+            <label for="fc_name">Nome</label>
+            <input type="text" name="name" id="fc_name" class="fc-input" placeholder="Jonh Due" required>
+        </div>
+    </div>
+
+    <div class="col-12">
+        <div class="fc-group">
+            <label for="fc_email">Email</label>
+            <input type="email" name="email" id="fc_email" class="fc-input" placeholder="jonh@email.com" required>
+        </div>
+    </div>
+
+    <div class="col-12 fc-margin-top-2">
+        <div class="fc-group">
+            <button class="fc-button" type="submit">Cadastrar</button>
+        </div>
+    </div>
 </form>
