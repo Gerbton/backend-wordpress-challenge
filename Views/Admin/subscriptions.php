b910b3f092096d5e409f29dba55df543d8f10797
Gerbton Fonseca <gerbton@gmail.com>
2021-10-06 23:39:19 -0300

    feat: add table to list subscriptions

---
+++ Views/Admin/subscriptions.php
@@ -0,0 +1,18 @@
+<table class="table table-striped">
+	<thead>
+	<tr>
+		<th scope="col">ID</th>
+		<th scope="col">Nome</th>
+		<th scope="col">E-mail</th>
+		<th scope="col">Registro</th>
+	</tr>
+	</thead>
+	<tbody>
+	<tr>
+		<th scope="row">1</th>
+		<td>Mark</td>
+		<td>gerbton@gmail.com</td>
+		<td>20/10/2021</td>
+	</tr>
+	</tbody>
+</table>
