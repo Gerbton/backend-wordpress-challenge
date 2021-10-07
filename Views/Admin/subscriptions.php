816c0c5eb91ad8ca3d39f72e6df157bae1b6c944
Gerbton Fonseca <gerbton@gmail.com>
2021-10-07 01:28:33 -0300

    feat: render subscriptions list

--- Views/Admin/subscriptions.php
+++ Views/Admin/subscriptions.php
@@ -1,18 +1,33 @@
-<table class="table table-striped">
-	<thead>
-	<tr>
-		<th scope="col">ID</th>
-		<th scope="col">Nome</th>
-		<th scope="col">E-mail</th>
-		<th scope="col">Registro</th>
-	</tr>
-	</thead>
-	<tbody>
-	<tr>
-		<th scope="row">1</th>
-		<td>Mark</td>
-		<td>gerbton@gmail.com</td>
-		<td>20/10/2021</td>
-	</tr>
-	</tbody>
-</table>
+<?php
+
+use App\Models\Subscription;
+
+$subscriptions = Subscription::findByPostID( $post->ID );
+
+if ( $subscriptions ) { ?>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Nome</th>
+            <th scope="col">E-mail</th>
+            <th scope="col">Registro</th>
+        </tr>
+        </thead>
+        <tbody>
+
+		<?php foreach ( $subscriptions as $subscription ) {
+			$registerDate = $subscription->getCreatedAt(); ?>
+
+            <tr>
+                <th scope="row"><?= $subscription->getId() ?></th>
+                <td><?= $subscription->getName() ?></td>
+                <td><?= $subscription->getEmail() ?></td>
+                <td><?= $registerDate instanceof DateTime ? $registerDate->format( 'd/m/Y' ) : '-' ?></td>
+            </tr>
+
+		<?php } ?>
+
+        </tbody>
+    </table>
+<?php }
