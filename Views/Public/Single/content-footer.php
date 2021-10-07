d275b0108cdd1731c778bc3b739c6dedab42f842
Gerbton Fonseca <gerbton@gmail.com>
2021-10-06 23:40:23 -0300

    refactor: improve code

--- Views/Public/Single/content-footer.php
+++ Views/Public/Single/content-footer.php
@@ -1,20 +1,16 @@
-<?php ?>
+<?php
+
+use App\Models\PostType\Post;
+
+$_post = new Post( get_post() );
+?>
 
 <div class="fc-container">
-    <h2 class="fc-title fc-title--form">Tenho interesse</h2>
-    <form action="" class="fc-d-flex" id="jsFuerzaSubscribeForm">
-        <input type="hidden" name="fc_nonce" value="<?= wp_create_nonce( 'wp_rest' ) ?>">
-        <input type="hidden" name="post_id" id="fc_post_id" value="<?= get_the_ID() ?>">
-        <div class="fc-group">
-            <label for="fc_name">Nome</label>
-            <input type="text" name="name" id="fc_name" class="fc_input" placeholder="Jonh Due">
-        </div>
-        <div class="fc-group">
-            <label for="fc_email">Email</label>
-            <input type="email" name="email" id="fc_email" class="fc_input" placeholder="jonh@email.com">
-        </div>
-        <div class="fc-group">
-            <button type="submit">Cadastrar</button>
-        </div>
-    </form>
+	<?php if ( $_post->isOpenToSubscribe() ) {
+		include "subscribe-form.php";
+	} ?>
+
+    <div <?= $_post->isOpenToSubscribe() ? 'hidden' : '' ?>>
+        <a href="<?= $_post->getLink() ?>" id="jsLinkToSubscribe" target="_blank">Cadastrar</a>
+    </div>
 </div>
---
+++ Views/Public/Single/subscribe-form.php
@@ -0,0 +1,16 @@
+<h2 class="fc-title fc-title--form">Tenho interesse</h2>
+<form action="" class="fc-d-flex" id="jsFuerzaSubscribeForm">
+	<input type="hidden" name="fc_nonce" value="<?= wp_create_nonce( 'wp_rest' ) ?>">
+	<input type="hidden" name="post_id" id="fc_post_id" value="<?= $_post->getID() ?>">
+	<div class="fc-group">
+		<label for="fc_name">Nome</label>
+		<input type="text" name="name" id="fc_name" class="fc_input" placeholder="Jonh Due">
+	</div>
+	<div class="fc-group">
+		<label for="fc_email">Email</label>
+		<input type="email" name="email" id="fc_email" class="fc_input" placeholder="jonh@email.com">
+	</div>
+	<div class="fc-group">
+		<button type="submit">Cadastrar</button>
+	</div>
+</form>
